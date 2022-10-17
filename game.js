32a809cec007ea856fe04371911b4e407d516cc4
Headpenguin <headpenguin.penguinarts@gmail.com>
2022-10-16 20:16:41 -0400

    Add text system

--- game.js
+++ game.js
@@ -1,5 +1,123 @@
+var masterText = [
+    "I do not remember my birth because it took place over the course of countlessly many thousands of years, but I do know that I was born as a result of water flowing through tiny cracks that cut deep through the Earth after earthquakes. As the water flowed, it left behind bits and pieces of me that came from the many minerals it encountered beforehand. Eventually, I was put together after millions of earthquakes occurred. For many more thousands of years, I was clueless of what lay at the Earth’s surface, entertaining myself with the many imperfections in the rock surrounding me and with the sensations of tremors in the ground and water trickling down my side, leaving behind more minerals which became a part of me. After a number of years, though, I ended up near the surface of the Earth next to the Kunlun Mountains in Mazongshan in a place which calls itself “中国”. “中” means middle, and “国” means country, so this place quite literally called itself the middle country.",
+    "One fateful day, a pit miner broke through the surface of the Earth which had protected me so well and revealed me for the first time to the sunlight. Even worse yet, the miner broke all the ground surrounding me and lugged me away. My first destination would be a warehouse, but my senses were too overloaded for me to have realized that at the time. After the horrible and completely unbearable year for which I sat in the warehouse, I was sent southwestward to Khotan, though I had no idea what trials I would undergo there.",
+    "Once I arrived, I met the Xinglongwa people, jade-carvers specializing in pendants. These people began their cruelty by cutting through me haphazardly. They had to struggle to cut me apart, hacking away and knocking bits and pieces of me out all over the place. At first, I felt a horrible sensation which I had never felt before, and as time went on, it only got worse. Eventually, after I felt like my life would surely end, I no longer felt pain. Did this mean I had finally died?",
+    "I only felt the sweet relief of death for a few moments after being set aside. The carver swiftly came back to me and did unspeakable things, crushing my hopes of being dead in the process. Though he was precise in how he cut me, it was nonetheless a horribly wretched experience, marred by the unrelenting cutting of my now tiny body. Despite the painful experience, I managed to survive, much to my disappointment. Though I did not know it at the time, I had become a marvelous jade dragon, destined to be buried with an official of the government of this mysterious “中国”.",
+    "After carving me, the carver sold me to a merchant, and thus began my final pilgrimage eastward to the funeral of the official. I hoped that the merchant would . . . (click one of the options below to change the outcome of the story)",
+    [
+        [
+            "Follow the military convoy ahead of us",
+            "Upon following the military convoy, my merchant was glad, as there were few oases across the massive desert. The merchant bringing me eastward was hoping to swap me out with a piece of white jade at the funeral, as that would be far more valuable. Once we arrived at our destination in Chang’an, I was met with the wonders of the beautiful mountains, protecting the city from invaders. I saw the countless Muslims, Buddhists, and Taoists roaming the streets. I even saw the Big Goose Pagoda in all of its glory, housing Buddhist scripts brought from India. However, it was time for the funeral of the official. My merchant was planning to disguise himself as a . . . "
+            [[
+                "Low ranking official",
+                "To achieve this, he got a futou as a disguise from some of the foreign traders he interacted with as a loan. From there, he could swap the white jade with me, and nobody would notice. Then, he would split the profits of the white jade with his accomplices after returning the disguise. However, there were also risks involved. It is extraordinarily rare for a merchant to attend a funeral, so for this merchant to get close enough to the jade to obtain it would be near impossible. Fortunately for me, though much to my dismay at the time, the merchant underestimated the attentiveness of the surrounding officials. Specifically, he assumed that the best way to go undetected was by disguising himself as the lowest official. He did not realize that since the lowest official would have known the most people at the funeral, the lowest official would be the most difficult to impersonate. The merchant could easily have pretended to be the emperor, whom very few people knew at the time anyway. Therefore, the merchant was caught and quickly punished, and I avoided being buried back into the earth where I had been just a year before. Since a search of the merchant quickly revealed my presence, the officials were suspicious that I was stolen, but upon taking inventory of the jade being prepared to be buried, they quickly deduced that the merchant had bought me with him. Believing me to be a source of evil, the officials quickly took me away from the palace and threw me into the hustling, bustling city.",
+                "After the experience with the funeral, I was quite miffed since I had not been returned to the Earth. However, a desperate civilian managed to find me, and this civilian quickly took me and brought me into his home. After bringing me back to his home, this civilian put me on his shelf in hopes that my holy nature may protect him. As he went to bed, I watched over him and had a view of his many siblings who joined him on the farm, pondering why I had been removed from the Earth.",
+                "For many more years, I watched over the same bed. I watched the young man’s parents die and how he took over the farm. I watched him get married and have children. I watched his excitement from having sons as children and his disappointment in daughters. I watched him die. For countless more generations, I guarded this farming family and watched it grow in prosperity, and the whole time, I continued to accept, slowly yet surely, my role as guardian. Just as I was beginning to get comfortable with my life of guarding these peasant farmers, disaster struck in the form of murder.",
+                "Though I did not realize at the time what the cause was, I knew that the people I was guarding, now several generations beyond the first civilian I met, did not come home, and I quickly deduced that they had died. At the time, there was great civil unrest, as the government was falling apart and being overthrown. The loneliness would have been unbearable, and at first it was, but much to my relief, one of the family members had managed to survive. The returning family member was a . . . ",
+                [[
+                    "Son",
+                ],[
+                    "Daughter",
+                    "This daughter was clearly in the most desperate of circumstances, not only left with no one to help her tend to the farm, but also left in the lowest of social positions. As a result, her next move was very understandable.",
+                    "The next day, I was in the hands of merchants who had managed to scrape together enough money to buy me. The farm daughter was left with no choice but to sell me in hopes of using the money to stay with another family in these desperate times, and she definitely needed the money because nobody would want to adopt a daughter. The merchants I was handed over to were intent on selling me to other people because they needed the money more than they needed a holy guardian. To sell me, though, the merchants needed to find others willing to buy me, a great difficulty in such tumultuous times. They looked toward Buddhists in a hope that the current lack of government may allow them to indulge in some precious jade without fear of persecution, but the Buddhists were running low on money to begin with, so they were most certainly not in a position to buy jade. The merchants attempted to sell me to the Manichaests, but the Manichaests saw me as a source of evil. Similar stories ensued for the Zoroastrinists and Nestorianists. Ultimately, the merchants were unable to sell me during this time of turmoil, but as their guardian, I felt I had the duty to protect them through powers that I scientifically knew I lacked but which I religiously felt desperate to believe in. By a miracle, the merchants managed to keep me, and knowing that I could bring prosperity to them, they rested, knowing their investment in me would pay off for their future children, who may even be able to become bureaucrats, should they have the skills and gifts in Confucianism. For nearly a century, I stayed in the hands of these traders and their children until a new government arose from the ashes of the previous one and things stabilized again.",
+                    "The traders continued trading, but one young trader in the family got the idea that he may be able to sell me to at least become a farmer instead of continuing to live the most lowly of lives. After being sold, I was now in the hands of a different eastbound trader, whom I watched over while he traveled eastward to the coast toward Quanzhou. Once he made it there, he quickly found a trader making the journey from Persia to here, to whom I was quickly sold. Due to the massive Himalayan Mountains, the journey ended up being more economical to make by sea than by land, but as a result, the trader could also bring many more goods than he otherwise would have been able to. As we returned to his ship, I was left in awe at the countless goods he had brought along for the journey: one Chinese junk full of cinnamon and other spices, one shipload of horses and slaves, and three more ships which were empty but presumably had loads more of materials that the trader managed to sell. I was moved into an empty ship and secured maximally so that no one may try to steal me. The trader stayed in Quanzhou for about a month more, and during that month, I was extraordinarily lonely. I only saw the trader at most once a day when he checked to make sure I was still in his ship. As time went on, he brought more similar jade carvings, but I longed for nothing more than to continue guarding people.",
+                    "That night, . . . ",
+                    [[
+                        "My ship was struck by lightning.",
+                    ],[
+                        "Absolutely nothing special happened",
+                        "Once the ship left, I felt the rockiness of the sea and the repeated banging of the wall, against my head, then against my tail, then once again against my head, and against my tail . . . ",
+                        "Over the course of my journey to Palembang, where the trader was planning to make a stop, I feared that I would shatter. Luckily, as a piece of jade, I had an obscenely high hardness, so I did not get so much as a scratch, and the carver who made me carved me in such a way that I would have no weak points that would result in me fracturing. After much time, we finally arrived in Palembang. Once we arrived, I was met with the sights of the one of the richest cities in the world. Here, gold bars were thrown into the ocean every day, countless traders were present, and there was plenty of food available for outgoing traders. Merchants rode in canoes ninety feet in length, and my owner clearly had a market to sell to. Of course, my owner was rather reluctant, as most traders here were Buddhists, not Muslim, but he still proceded to trade what he had anyways. The whole time, I was hoping my owner would not find another Muslim to trade with, since if he did, it would be far more likely that he would attempt to trade me as well. Luckily for me, he . . . ",
+                        [[
+                            "Did find another Muslim trader",
+                            "Upon finding this trader, I was quickly sold. This new trader, though, was unfortunately a clumsy trader, and while taking me to the place where he slept, he accidentally dropped me, at which point I fell to the bottom of the sea."
+                        ],[
+                            "Did not find another Muslim trader",
+                            "Now, he was ready to leave, as the monsoon winds which would carry his boat home would wait for nobody. He had managed to restock his spices after selling them in “中国” and bought some additional odd commodities, such as areca nuts, rhinoceros horns, and turtle shells, all of which had been imported from Javanese markets. The wealth which this trader had amassed appeared to be immeasurable to me, so I was glad that I could join him for the ride.",
+                            "Before moving on, though, my owner returned the Chinese junks to the dock and swapped them out for his dhows, a freedom which signaled his great wealth. These new boats were held together not by nails, but by string. The hulls were sewn together, and the sails enabled these boats to maximally take advantage of the monsoon winds. The journey to Mangalore was long, but by now, I had grown used to such journeys, and before I knew it, I was already in Mangalore.",
+                            "Spices were not rare; many spice traders gathered near Malabar coast, so trade in Mangalore was difficult for my owner. My owner also needed to be careful, due to the pirates that frequented the Netravati River, which flows through Mangalore. My owner disembarked with some of his goods to trade inside the city. As we went into the city, we saw goods such as indigo and ivory. We also saw fish markets run by the Mogaveera people, who are, unsurprisingly, fishers. They continued fishing despite all the obstacles they faced, whether that be war or competition in the fish markets. We also saw the farms of the Billiva people, who were known for their medicine and great loyalty. In fact, we even saw one such Billiva helping to cure a Brahmin despite the rigid oppositions that have come to be expected of Brahmins against interaction between different castes. The Nadava people too were farmers, though their farms were far more breath-taking than those of the Billivas due to their generally higher position in the caste structure. Some Nadavas, though, did not adhere to such structures, as they were Jainists, not Hindus. As we returned to the ship, my owner placed me down and left me during the night before his departure the next day. Unfortunately, . . . ",
+                            [[
+                                "My owner seemed to be ignorant of the many pirates who were attracted to the great wealth of his ship.",
+                                "I was jolted to the sound of wood smashing in the morning and was helpless as my captors secured me in their boat and departed to Bukhara.",
+                                "Upon arrival in Bukhara, part of the Muslim world, eager Persian, Indian, Chinese, and Russian merchants awaited to trade. I saw fleeces, silk, cotton, leather, carpets, clothing, and a wide variety of goods. I saw signs of religion in the Kalyan mosque, minaret, and the Mir-i Arab madrassah, a religious school. I saw advanced architecture and urban structure. I did not stay long; my captors quickly began attempting to sell me. As I watched, many traders had never seen jade before and were astounded by my beauty. I was eventually sold to a merchant who wanted to return to Kashgar in “中国” and, while there, sell me for a high price, since jade was valued in “中国” for being a symbol of the elite.",
+                                "I was alone with my thoughts as I traveled to Kashgar. I was angry and tired of being traded around and dreamt of continuing my role as a guardian. Once we arrived at Kashgar, my weariness began setting in. I no longer was astounded by the pottery and textiles that were traded here, nor was I astounded by the Yuezhi people or their cultural practices. The curiosity of the fact that there were Turkik people brought no entertainment to me. The government’s frequent upheaval and the never ending war between the Muslim and Chinese rulers brought no fear to my mind. All I wanted was to be a guardian, to feel that I was worth something, and these feelings finally came to fruition when a native bought me. In the home of this farmer who cultivated the most fertile lands, I was at last returned to the peaceful lifestyle for which I longed since I was sold by the last family I guarded. Upon finding my peace here, I was happy, and I continued living my peaceful life, invasion or not, up until the end of Mongol rule."
+                            ],[
+                                "My owner spent too long in the city, and he would be cutting the timing with the Monsoon winds very close.",
+                            ]]
+                        ]]
+
+                    ]]
+                ]]
+            ],[
+                "High ranking official",
+            ]]
+        ], 
+        [
+            "Stay away from the military convoy ahead of us"
+        ]
+    ]
+];
+var currentText = 0;
+var currentList = masterText;
+var nextButton;
+var prevButton;
+var out;
+var textBox;
+var initialTextBoxLength;
+
+function nextText() {
+    out.innerHTML = currentList[currentText]; 
+    if(currentText > 0) prevButton.disabled = false;
+    currentText++;
+    if(currentText === currentList.length - 1) {
+        nextButton.disabled = true;
+        prevButton.disabled = true;
+        console.log(currentText);
+        textBox.innerHTML += "<button id=\"optionA\"onclick=\"option1()\">" + currentList[currentText][0][0] + "</button>" +
+        "<button id=\"optionB\"onclick=\"option2()\">" + currentList[currentText][1][0] + "</button>";
+    }
+}
+
+function prevText() {
+    currentText--;
+    nextButton.disabled = false;
+    if(currentText <= 0) prevButton.disabled = true
+    out.innerHTML = currentList[currentText];
+}
+
+function option1() {
+    textBox.removeChild(document.getElementById("optionA"));
+    textBox.removeChild(document.getElementById("optionB"));
+    nextButton = document.getElementById("next");
+    prevButton = document.getElementById("prev");
+    out = document.getElementById("Text");
+    currentList = currentList[currentText][0];
+    currentText = 1;
+    nextButton.disabled = false;
+    out.innerHTML = currentList[currentText];
+}
+
+function option2() {
+    textBox.removeChild(document.getElementById("optionA"));
+    textBox.removeChild(document.getElementById("optionB"));
+    nextButton = document.getElementById("next");
+    prevButton = document.getElementById("prev");
+    out = document.getElementById("Text");
+    currentList = currentList[currentText][1];
+    currentText = 1;
+    nextButton.disabled = false;
+    out.innerHTML = currentList[currentText];
+}
+
 function init() {
     var canvas = document.getElementById("canvas");
+    nextButton = document.getElementById("next");
+    prevButton = document.getElementById("prev");
+    out = document.getElementById("Text");
+    textBox = document.getElementById("Textbox");
+    initialTextBoxLength = textBox.length;
+    out.innerHTML = "Welcome! Hit the \"next\" button to proceed and the \"previous\" button to go back";
+    prevButton.disabled = true;
     var ctx = canvas.getContext('2d');
     ctx.fillStyle = '#F0DB4F';
     ctx.strokeStyle = 'red';
